878f7e3cd006f051198d3dcd7ec1892940ddd7bf
Ewa Kadziolka <kalistaewa@gmail.com>
2022-11-02 16:03:15 +0100

    adds messages json

--- mpa-spa/src/init.js
+++ mpa-spa/src/init.js
@@ -1,6 +1,8 @@
+import { initMessages } from "./messages.js";
 import { renderMenu } from "./components/menu.js";
 import{ route } from "./router.js"
 import{renderFooter} from "./components/footer.js"
+await initMessages()
 renderMenu();
 renderFooter();
 route();
--- mpa-spa/src/messages.js
+++ mpa-spa/src/messages.js
@@ -1,9 +1,8 @@
-const messages = {
-    brand:"SPA",
-    home:{menuItem:"Home", h1:"Welcome to SPA demo", h2:"Here you will read what we do"},
-    contact: {menuItem: "Contact", h1: "Contact"},
-    signIn: {menuItem:"Sign-in", h1: "Sign in"},
-    about: {menuItem:"About", h1: "About", h2: "Here you will read who we are"},
-    error:{h1:"ERROR 404", h2:"This page doesn't exist"}
-};
-export{messages}
+let messages = null;
+
+async function initMessages() {
+    const response = await fetch ("./src/resources/messages.json");
+    messages = await response.json();
+}
+
+export{initMessages, messages}
